b54aaee1f57ac24a203868a1903420395ffa8d77
mozjiang <dance.jms@gmail.com>
2018-10-11 06:30:56 +0800

    add author

--- popup.js
+++ popup.js
@@ -18,31 +18,26 @@ function httpRequest(url, callback){
 
 
 function copyTextToClipboard(text) {
-  //Create a textbox field where we can insert text to. 
+
   console.log(text);
   var copyFrom = document.createElement("textarea");
 
   copyFrom.contentEditable = true;
   copyFrom.setAttribute("style","position:absolute; left: 0; top: 0; height: 0; visability: hidden;");
-  //Set the text content to be the text you wished to copy.
   copyFrom.textContent = text;
 
-  //Append the textbox field into the body as a child. 
   //"execCommand()" only works when there exists selected text, and the text is inside 
   //document.body (meaning the text is part of a valid rendered HTML element).
   document.body.appendChild(copyFrom);
 
   //Select all the text!
   copyFrom.select();
-
-  //Execute command
   document.execCommand('copy');
 
   //(Optional) De-select the text using blur(). 
   copyFrom.blur();
 
   document.body.removeChild(copyFrom);
-
   //Remove the textbox field from the document.body, so no other JavaScript nor 
   //other elements can get access to this.
   // document.body.removeChild(copyFrom);
@@ -92,7 +87,7 @@ function handleHideButton(){
   }, false)
 }
 
-function button(){
+function handleSpecificIp(){
   var el = document.getElementById('specific-ip-submit');
   el.addEventListener('click', function(){
     var ip = document.getElementById('specific-ip').value;
@@ -125,5 +120,6 @@ function showIp(url){
 handleCp();
 handleInstantCp();
 handleHideButton();
-button();
+handleSpecificIp();
+
 showIp(ownIpQueryUrl);
